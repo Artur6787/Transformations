a74fa925b7a7a47e5be62c6602b98d267f693f33
Artur6787 <starx152@gmail.com>
2024-11-22 14:10:55 +0300

    Init

--- Assets/Scripts/SizeIncreaser.cs
+++ Assets/Scripts/SizeIncreaser.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SizeIncreasing : MonoBehaviour
+public class SizeIncreaser : MonoBehaviour
 {
-    [SerializeField] private Transform _follower;
     [SerializeField] private float _speed;
 
     private void Update()
     {
-        var direction = (_follower.position - transform.position).normalized;
         transform.localScale += Vector3.one * _speed * Time.deltaTime;
-        transform.LookAt(_follower);
     }
 }
