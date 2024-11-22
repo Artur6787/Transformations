cd41d85b6c99710645d99d18e058c42c703305c0
Artur6787 <starx152@gmail.com>
2024-11-22 22:29:53 +0300

    Init

--- Assets/Scripts/Rotater.cs
+++ Assets/Scripts/Rotater.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Rotater : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Transform _rotationCenter;
+    [SerializeField] private Vector3 _offset;
 
+    private void Start()
+    {
+        _offset = transform.position - _rotationCenter.position;
+    }
     private void Update()
     {
-        transform.Rotate(Vector3.up * _speed * Time.deltaTime);
+        transform.position = _rotationCenter.position + _offset;
+        transform.RotateAround(_rotationCenter.position, Vector3.up, _speed * Time.deltaTime);
+        _offset = transform.position - _rotationCenter.position;
     }
 }
